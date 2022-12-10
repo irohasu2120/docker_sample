e7c21d824091ff5b50a22fa28772086e7be93d0d
irohasu2120 <halo1991sa@gmail.com>
2022-12-10 13:45:25 +0900

    add: lof4j2を追加

--- java_mysql_jenkins/myapp/README.md
+++ java_mysql_jenkins/myapp/README.md
@@ -1,5 +1,13 @@
 # MyApp
 
+## Intellij
+### コンソール文字化け対策
+コンソールの日本語が文字化けしている場合、以下の設定を追記する。
+```
+[ヘルプ]-[カスタムVMオプションの編集]
+-Dfile.encoding=UTF-8
+```
+
 ## Swagger
 ### Swagger-UI
 http://{Domain}/swagger-ui.html
--- java_mysql_jenkins/myapp/src/main/java/com/github/irohasu2120/myapp/sample/SampleRestApiServiceImpl.java
+++ java_mysql_jenkins/myapp/src/main/java/com/github/irohasu2120/myapp/sample/SampleRestApiServiceImpl.java
@@ -3,9 +3,11 @@ package com.github.irohasu2120.myapp.sample;
 import com.github.irohasu2120.myapp.domain.Sample_T;
 import com.github.irohasu2120.myapp.sample.dto.Person;
 import com.github.irohasu2120.myapp.sample.repository.SampleCustomRepository;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+@Log4j2
 @Service
 public class SampleRestApiServiceImpl implements SampleRestApiServe {
 
@@ -16,7 +18,7 @@ public class SampleRestApiServiceImpl implements SampleRestApiServe {
     @Override
     public Person doProcess(String id) {
         Sample_T sample_t = sampleCustomRepository.findById(Integer.parseInt(id));
-
+        log.info("ログ出てるね");
         if (sample_t != null) {
             return new Person(String.valueOf(sample_t.getSampleId()), sample_t.getSampleName());
         } else {
