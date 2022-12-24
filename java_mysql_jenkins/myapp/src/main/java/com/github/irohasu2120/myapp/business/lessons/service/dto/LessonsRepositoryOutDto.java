30582313722eb85c1dc65c6211836d84ac22cb32
irohasu2120 <halo1991sa@gmail.com>
2022-12-25 07:59:16 +0900

    add: 授業一覧APIテストクラス作成

--- java_mysql_jenkins/myapp/src/main/java/com/github/irohasu2120/myapp/business/lessons/service/dto/LessonsRepositoryOutDto.java
+++ java_mysql_jenkins/myapp/src/main/java/com/github/irohasu2120/myapp/business/lessons/service/dto/LessonsRepositoryOutDto.java
@@ -1,8 +1,12 @@
 package com.github.irohasu2120.myapp.business.lessons.service.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class LessonsRepositoryOutDto {
     /** 授業ID */
     private int lessonId;
--- java_mysql_jenkins/myapp/src/main/java/com/github/irohasu2120/myapp/domain/Lesson.java
+++ java_mysql_jenkins/myapp/src/main/java/com/github/irohasu2120/myapp/domain/Lesson.java
@@ -1,15 +1,16 @@
 package com.github.irohasu2120.myapp.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 
-import java.sql.Date;
 import java.time.LocalDateTime;
 
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
+@AllArgsConstructor
 public class Lesson extends BaseSingleDomain {
     /** 授業ID */
     private int lessonId;
@@ -18,7 +19,7 @@ public class Lesson extends BaseSingleDomain {
     private String lessonName;
 
     /** 公開開始日 */
-    private Date openDate;
+    private LocalDateTime openDate;
 
     /** 公開終了日 */
     private LocalDateTime closeDate;
